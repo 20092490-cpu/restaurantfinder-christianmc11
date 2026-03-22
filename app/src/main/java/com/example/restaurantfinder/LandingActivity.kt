11891e096165a1fb7e42274d6f9d74014fb2e5e5
Chris <chrisjohnmcc@gmail.com>
2026-03-22 12:36:27 +0000

    Redesigned UI

--- app/src/main/java/com/example/restaurantfinder/LandingActivity.kt
+++ app/src/main/java/com/example/restaurantfinder/LandingActivity.kt
@@ -1,4 +1,27 @@
 package com.example.restaurantfinder
 
-class LandingActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.Button
+import android.view.View
+
+class LandingActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_landing)
+
+        val myButton = findViewById<Button>(R.id.button_go_home)
+
+        myButton.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View?) {
+
+                val myIntent = Intent(this@LandingActivity, MainActivity::class.java)
+
+                startActivity(myIntent)
+            }
+        })
+    }
 }
--- app/src/main/java/com/example/restaurantfinder/MainActivity.kt
+++ app/src/main/java/com/example/restaurantfinder/MainActivity.kt
@@ -1,47 +1,30 @@
 package com.example.restaurantfinder
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.restaurantfinder.ui.theme.RestaurantFinderTheme
+import androidx.appcompat.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            RestaurantFinderTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
-    }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+        setContentView(R.layout.activity_main)
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    RestaurantFinderTheme {
-        Greeting("Android")
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val restaurantsList = arrayOf(
+            "Kamboat - The Quay",
+            "Indian Ocean -456 Street",
+            "Pearls - Main Street"
+        )
+
+        val myListView = findViewById<ListView>(R.id.list_restaurants)
+
+        val myAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, restaurantsList)
+
+        myListView.adapter = myAdapter
     }
 }
